26817a5d5d236a4f097ec23663f7d4e123a4d7db
Tsegaye Shumet <tsegashu@gmail.com>
2026-08-20 15:50:52 +0300

    Fix strict TypeScript types in WebGL geometry and Framer Motion variants

--- components/FiberBackground.tsx
+++ components/FiberBackground.tsx
@@ -39,9 +39,6 @@ const Particles = () => {
       <bufferGeometry>
         <bufferAttribute
           attach="attributes-position"
-          count={positions.length / 3}
-          array={positions}
-          itemSize={3}
           args={[positions, 3]}
         />
       </bufferGeometry>
